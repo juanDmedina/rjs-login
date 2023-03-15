8ef5e74f9f8d6283224a6cca6ae3aef434e6598f
Juan Medina Rojas <juan.medina@sofka.local>
2023-03-15 12:07:16 -0500

    Add ---- initial strucure and api

---
+++ rjslogin/src/api/loginApi.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const baseURL = 'http://192.168.1.75:8080/api';
+
+const loginApi = axios.create({ baseURL });
+
+loginApi.interceptors.request.use(
+    async(config) => {
+        const token = localStorage.getItem('token');
+        if ( token ) {
+            config.headers['x-token'] = token;
+        }
+        return config;
+    }
+);
+
+
+
+export default loginApi
